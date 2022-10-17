d394b519b75ab0030014f38960e7c24c94da0c1c
ahmetson <ahmetson@zoho.com>
2022-10-17 14:09:05 +0300

    add static.Abi

---
+++ static/abi.go
@@ -0,0 +1,53 @@
+package static
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type Abi struct {
+	Bytes []byte
+	// Body abi.ABI
+	Body    interface{}
+	AbiHash string
+	exists  bool
+}
+
+func (a *Abi) CalculateAbiHash() {
+	hash := crypto.Keccak256Hash(a.Bytes)
+	a.AbiHash = hash.String()[2:10]
+}
+
+func (a *Abi) Exists() bool {
+	return a.exists
+}
+
+func (a *Abi) SetExists(exists bool) {
+	a.exists = exists
+}
+
+func BuildAbi(body interface{}) *Abi {
+	abi := Abi{Body: body, exists: false}
+	byt, err := json.Marshal(body)
+	if err != nil {
+		return &abi
+	}
+
+	abi.Bytes = byt
+	abi.CalculateAbiHash()
+
+	return &abi
+}
+
+func BuildAbiByBytes(bytes []byte) *Abi {
+	body := []interface{}{}
+	err := json.Unmarshal(bytes, &body)
+	if err != nil {
+		fmt.Println("Failed to convert abi bytes to body interface")
+	}
+
+	abi := Abi{Body: body, exists: false, Bytes: bytes}
+	return &abi
+}
