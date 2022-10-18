28e72ee9a77b0ef45dcb92d7d310dc48cce8291a
ahmetson <ahmetson@zoho.com>
2022-10-18 17:38:52 +0300

    add categorizer transaction

--- categorizer/transaction.go
+++ categorizer/transaction.go
@@ -1,4 +1,18 @@
 package categorizer
 
 type Transaction struct {
+	ID          uint64
+	NetworkId   string
+	Address     string
+	BlockNumber int
+	Txid        string
+	TxIndex     uint
+	TxFrom      string
+	Method      string
+	Args        map[string]interface{}
+	Value       float64
+}
+
+func (b *Transaction) Key() string {
+	return b.NetworkId + "." + b.Address
 }
