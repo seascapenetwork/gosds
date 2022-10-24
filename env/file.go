47d1d895a155e3bf004b36fbb6c27a6d7d750764
ahmetson <ahmetson@zoho.com>
2022-10-24 16:21:30 +0300

    Add loading .env and make it optional

---
+++ env/file.go
@@ -0,0 +1,36 @@
+/* The environment package's file category handles loading
+.env or any other environment variable that is provided by the user
+*/
+package env
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func optionalPaths() ([]string, error) {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		return nil, nil
+	}
+
+	return args, nil
+}
+
+func LoadAnyEnv() error {
+	opts, optErr := optionalPaths()
+	if optErr != nil {
+		return optErr
+	}
+
+	dotErr := godotenv.Load()
+	if dotErr != nil {
+		return dotErr
+	}
+
+	if opts != nil {
+		return godotenv.Load(opts...)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/pebbe/zmq4 v1.2.9 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
