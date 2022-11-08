ec90ec477051f1a08c826013efb229c19313bab5
ahmetson <ahmetson@zoho.com>
2022-11-08 18:47:52 +0300

    add logs fetching by tx id in the database

--- categorizer/log.go
+++ categorizer/log.go
@@ -24,6 +24,65 @@ func (b *Log) Key() string {
 	return b.NetworkId + "." + b.Address
 }
 
+func (b *Log) ToJSON() map[string]interface{} {
+	i := map[string]interface{}{}
+	i["network_id"] = b.NetworkId
+	i["txid"] = b.Txid
+	i["log_index"] = b.LogIndex
+	i["address"] = b.Address
+	i["log"] = b.Log
+	i["output"] = b.Output
+	return i
+}
+
+func ParseLog(blob map[string]interface{}) *Log {
+	return &Log{
+		NetworkId: blob["network_id"].(string),
+		Txid:      blob["txid"].(string),
+		LogIndex:  blob["log_index"].(uint),
+		Address:   blob["address"].(string),
+		Log:       blob["log"].(string),
+		Output:    blob["output"].(map[string]interface{}),
+	}
+}
+
+func RemoteLogs(socket *zmq.Socket, txids []string) ([]*Log, error) {
+	request := message.Request{
+		Command: "log_get_all",
+		Param: map[string]interface{}{
+			"txids": txids,
+		},
+	}
+	fmt.Println("Sending message to SDS Log server to parse log. The mesage sent to server")
+	fmt.Println(request.ToString())
+	if _, err := socket.SendMessage(request.ToString()); err != nil {
+		fmt.Println("Failed to send a command for smartcontracts getting from SDS Log", err.Error())
+		return nil, err
+	}
+
+	// Wait for reply.
+	r, err := socket.RecvMessage(0)
+	if err != nil {
+		fmt.Println("Failed to receive reply from static controller", err.Error())
+		return nil, err
+	}
+
+	reply, err := message.ParseReply(r)
+	if err != nil {
+		fmt.Println("Failed to parse smartcontracts reply", err.Error())
+		return nil, err
+	}
+	if !reply.IsOK() {
+		fmt.Println("The static server returned failure: ", reply.Message)
+		return nil, err
+	}
+
+	return reply.Params["name"].(string), reply.Params["args"], nil
+}
+
+// parse the raw event data from spaghetti using SDS Log
+// parsing events using JSON abi is harder in golang, therefore we use javascript
+// implementation called SDS Log.
 func RemoteParse(socket *zmq.Socket, networkId string, address string, data string, topics []string) (string, interface{}, error) {
 	request := message.Request{
 		Command: "parse",
