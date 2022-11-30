3f427e802f4058acc6b989b227f6a3abe5d7434c
ahmetson <ahmetson@zoho.com>
2022-11-30 15:13:54 +0300

    Add a function that returns the remote block range from spaghetti

--- spaghetti/block.go
+++ spaghetti/block.go
@@ -25,3 +25,47 @@ func RemoteBlockMintedTime(socket *remote.Socket, networkId string, blockNumber 
 
 	return blockTimestamp, nil
 }
+
+func RemoteBlockRange(socket *remote.Socket, networkId string, address string, from uint64, to uint64) (uint64, []Transaction, []Log, error) {
+	request := message.Request{
+		Command: "block_get_range",
+		Param: map[string]interface{}{
+			"block_number_from": from,
+			"block_number_to":   to,
+			"to":                address,
+			"network_id":        networkId,
+		},
+	}
+
+	params, err := socket.RequestRemoteService(&request)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	timestamp, err := message.GetUint64(params, "timestamp")
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	raw_transactions, err := message.GetMapList(params, "transactions")
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	raw_logs, err := message.GetMapList(params, "logs")
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	transactions := make([]Transaction, len(raw_transactions))
+	for i, raw := range raw_transactions {
+		transactions[i] = ParseTransaction(raw)
+	}
+
+	logs := make([]Log, len(raw_logs))
+	for i, raw := range raw_logs {
+		logs[i] = ParseLog(raw)
+	}
+
+	return timestamp, transactions, logs, nil
+}
