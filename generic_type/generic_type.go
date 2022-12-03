7315290a210ecbfd20d35fb36c971d0e4233869e
ahmetson <ahmetson@zoho.com>
2022-12-03 19:42:48 +0300

    Add a generic type function to create list of strings

--- generic_type/generic_type.go
+++ generic_type/generic_type.go
@@ -15,13 +15,28 @@ type SDS_Data interface {
 	ToJSON() map[string]interface{}
 }
 
-// Converts the data structs to the JSON objects (represented as a golang map) list.
-func ToMapList[V SDS_Data](list []V) []map[string]interface{} {
+type SDS_String_Data interface {
+	*static.SmartcontractKey
+}
 
+// Converts the data structs to the JSON objects (represented as a golang map) list.
+// []map[string]interface{}
+func ToMapList[V SDS_Data](list []V) []map[string]interface{} {
 	map_list := make([]map[string]interface{}, len(list))
 	for i, element := range list {
 		map_list[i] = element.ToJSON()
 	}
 
 	return map_list
+}
+
+// Converts the data structs to the list of strings.
+// []string
+func ToStringList[V SDS_String_Data](list []V) []string {
+	string_list := make([]string, len(list))
+	for i, element := range list {
+		string_list[i] = string(*element)
+	}
+
+	return string_list
 }
