7362c582f3f3c1e0d7cc3d408aa74a34ff8a25c8
ahmetson <ahmetson@zoho.com>
2022-12-08 00:04:57 +0300

    Reply controller depending on the command handler parses different requests

--- controller/reply_controller.go
+++ controller/reply_controller.go
@@ -8,6 +8,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/blocklords/gosds/account"
 	"github.com/blocklords/gosds/env"
 	"github.com/blocklords/gosds/message"
 
@@ -16,9 +17,10 @@ import (
 
 type CommandHandlers map[string]interface{}
 
-/*
-Creates a new Reply controller using ZeroMQ
-*/
+type RequestCommandHandler func(*sql.DB, message.Request) message.Reply
+type SmartcontractDeveloperRequestCommandHandler func(*sql.DB, message.SmartcontractDeveloperRequest, *account.SmartcontractDeveloper) message.Reply
+
+// Creates a new Reply controller using ZeroMQ
 func ReplyController(db *sql.DB, commands CommandHandlers, e *env.Env) {
 	if !e.PortExist() {
 		panic(fmt.Errorf("missing .env variable: Please set '" + e.ServiceName() + "' port"))
@@ -55,6 +57,7 @@ func ReplyController(db *sql.DB, commands CommandHandlers, e *env.Env) {
 			continue
 		}
 
+		// Any request types is compatible with the Request.
 		if commands[request.Command] == nil {
 			fail := message.Fail("invalid command " + request.Command)
 			reply := fail.ToString()
@@ -64,7 +67,34 @@ func ReplyController(db *sql.DB, commands CommandHandlers, e *env.Env) {
 			continue
 		}
 
-		reply := commands[request.Command].(func(*sql.DB, message.Request) message.Reply)(db, request)
+		var reply message.Reply
+
+		command_handler, ok := commands[request.Command].(SmartcontractDeveloperRequestCommandHandler)
+		if ok {
+			smartcontract_developer_request, err := message.ParseSmartcontractDeveloperRequest(msg_raw)
+			if err != nil {
+				fail := message.Fail("invalid smartcontract developer request " + err.Error())
+				reply := fail.ToString()
+				if _, err := socket.SendMessage(reply); err != nil {
+					println(fmt.Errorf("sending reply: %w", err))
+				}
+				continue
+			}
+
+			smartcontract_developer, err := smartcontract_developer_request.GetAccount()
+			if err != nil {
+				fail := message.Fail("invalid smartcontract developer request " + err.Error())
+				reply := fail.ToString()
+				if _, err := socket.SendMessage(reply); err != nil {
+					println(fmt.Errorf("sending reply: %w", err))
+				}
+				continue
+			}
+
+			reply = command_handler(db, smartcontract_developer_request, smartcontract_developer)
+		} else {
+			reply = commands[request.Command].(RequestCommandHandler)(db, request)
+		}
 
 		if _, err := socket.SendMessage(reply.ToString()); err != nil {
 			println(fmt.Errorf("error sending controller reply: %w", err))
