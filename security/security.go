b1756eabf1840b7310581467d46ba5cf10c564c4
ahmetson <ahmetson@zoho.com>
2023-01-12 16:39:31 +0300

    security package checks environment variables automatically

--- security/security.go
+++ security/security.go
@@ -2,17 +2,26 @@
 // Requests secure.
 package security
 
+import (
+	"github.com/blocklords/gosds/env"
+)
+
 const PLAIN = "plain"
 
 // Whether the socket connections are in plain or not.
 // Accepts the list of the arguments passed to the application.
 // If one of the arguments is "--plain", then this application runs in a plain mode.
-func IsPlain(arguments []string) bool {
+func IsPlain() (bool, error) {
+	arguments, err := env.LoadArguments()
+	if err != nil {
+		return false, err
+	}
+
 	for _, arg := range arguments {
 		if arg == PLAIN {
-			return true
+			return true, nil
 		}
 	}
 
-	return false
+	return false, nil
 }
