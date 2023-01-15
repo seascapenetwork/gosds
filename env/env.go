ca7fda7773a4b8879ccc09b1e88370126a656b97
ahmetson <ahmetson@zoho.com>
2023-01-15 18:27:03 +0300

    environment package checks the PLAIN argument before validation public key or private key

--- env/env.go
+++ env/env.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/blocklords/gosds/argument"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
@@ -29,18 +30,18 @@ type Env struct {
 	broadcast_secret_key string
 }
 
-// Checks whether the envrionment variable exists or not
+// Checks whether the environment variable exists or not
 func Exists(name string) bool {
 	_, exists := os.LookupEnv(name)
 	return exists
 }
 
-// Returns an envrionment variable as a string
+// Returns an environment variable as a string
 func GetString(name string) string {
 	return os.Getenv(name)
 }
 
-// Returns an envrionment variable as a number
+// Returns an environment variable as a number
 func GetNumeric(name string) uint {
 	value := os.Getenv(name)
 	if len(value) == 0 {
@@ -55,35 +56,35 @@ func GetNumeric(name string) uint {
 	return uint(num)
 }
 
-// Returns the envrionment variable for the SDS Spaghetti
-func Spaghetti() *Env { return Get("SPAGHETTI") }
+// Returns the environment variable for the SDS Spaghetti
+func Spaghetti() (*Env, error) { return Get("SPAGHETTI") }
 
-// Returns the envrionment variable for the SDS Categorizer
-func Categorizer() *Env { return Get("CATEGORIZER") }
+// Returns the environment variable for the SDS Categorizer
+func Categorizer() (*Env, error) { return Get("CATEGORIZER") }
 
-// Returns the envrionment variable for the SDS Static
-func Static() *Env { return Get("STATIC") }
+// Returns the environment variable for the SDS Static
+func Static() (*Env, error) { return Get("STATIC") }
 
-// Returns the envrionment variable for the SDS Gateway
-func Gateway() *Env { return Get("GATEWAY") }
+// Returns the environment variable for the SDS Gateway
+func Gateway() (*Env, error) { return Get("GATEWAY") }
 
-// Returns the envrionment variable for the SDS Developer Gateway
-func DeveloperGateway() *Env { return Get("DEVELOPER_GATEWAY") }
+// Returns the environment variable for the SDS Developer Gateway
+func DeveloperGateway() (*Env, error) { return Get("DEVELOPER_GATEWAY") }
 
-// Returns the envrionment variable for the SDS Publisher
-func Publisher() *Env { return Get("PUBLISHER") }
+// Returns the environment variable for the SDS Publisher
+func Publisher() (*Env, error) { return Get("PUBLISHER") }
 
-// Returns the envrionment variable for the SDS Reader
-func Reader() *Env { return Get("READER") }
+// Returns the environment variable for the SDS Reader
+func Reader() (*Env, error) { return Get("READER") }
 
-// Returns the envrionment variable for the SDS Writer
-func Writer() *Env { return Get("WRITER") }
+// Returns the environment variable for the SDS Writer
+func Writer() (*Env, error) { return Get("WRITER") }
 
-// Returns the envrionment variable for the SDS Bundler
-func Bundle() *Env { return Get("BUNDLE") }
+// Returns the environment variable for the SDS Bundler
+func Bundle() (*Env, error) { return Get("BUNDLE") }
 
-// Returns the envrionment variable for the SDS Log
-func Log() *Env { return Get("LOG") }
+// Returns the environment variable for the SDS Log
+func Log() (*Env, error) { return Get("LOG") }
 
 func NewDeveloper(public_key string, secret_key string) *Env {
 	return &Env{
@@ -99,23 +100,45 @@ func NewDeveloper(public_key string, secret_key string) *Env {
 	}
 }
 
-func Developer() *Env {
+func Developer() (*Env, error) {
+	exist, err := argument.Exist(argument.PLAIN)
+	if err != nil {
+		return nil, err
+	}
+	if exist {
+		return NewDeveloper("", ""), nil
+	}
+	if !Exists("DEVELOPER_PUBLIC_KEY") || !Exists("DEVELOPER_SECRET_KEY") {
+		return nil, errors.New("missing 'DEVELOPER_PUBLIC_KEY' or 'DEVELOPER_SECRET_KEY'")
+	}
 	public_key := GetString("DEVELOPER_PUBLIC_KEY")
 	secret_key := GetString("DEVELOPER_SECRET_KEY")
 
-	return NewDeveloper(public_key, secret_key)
+	return NewDeveloper(public_key, secret_key), nil
 }
 
 // for example env.Get("SPAGHETTI").
-func Get(service string) *Env {
+func Get(service string) (*Env, error) {
 	host := GetString(service + "_HOST")
 	port := GetString(service + "_PORT")
 	broadcast_host := GetString(service + "_BROADCAST_HOST")
 	broadcast_port := GetString(service + "_BROADCAST_PORT")
-	public_key := GetString(service + "_PUBLIC_KEY")
-	secret_key := GetString(service + "_SECRET_KEY")
-	broadcast_public_key := GetString(service + "_BROADCAST_PUBLIC_KEY")
-	broadcast_secret_key := GetString(service + "_BROADCAST_SECRET_KEY")
+	public_key := ""
+	secret_key := ""
+	broadcast_public_key := ""
+	broadcast_secret_key := ""
+
+	exist, err := argument.Exist(argument.PLAIN)
+	if err != nil {
+		return nil, err
+	}
+	if !exist {
+		public_key = GetString(service + "_PUBLIC_KEY")
+		secret_key = GetString(service + "_SECRET_KEY")
+		broadcast_public_key = GetString(service + "_BROADCAST_PUBLIC_KEY")
+		broadcast_secret_key = GetString(service + "_BROADCAST_SECRET_KEY")
+
+	}
 
 	return &Env{
 		service:              service,
@@ -127,7 +150,7 @@ func Get(service string) *Env {
 		secret_key:           secret_key,
 		broadcast_public_key: broadcast_public_key,
 		broadcast_secret_key: broadcast_secret_key,
-	}
+	}, nil
 }
 
 // Returns the service environment parameters by its Public Key
@@ -146,7 +169,10 @@ func GetByPublicKey(public_key string) (*Env, error) {
 	}
 
 	for _, service := range services {
-		service_env := Get(service)
+		service_env, err := Get(service)
+		if err != nil {
+			return nil, err
+		}
 		if service_env != nil && service_env.public_key == public_key {
 			return service_env, nil
 		}
@@ -226,31 +252,72 @@ func (e *Env) BroadcastPort() string {
 	return e.broadcast_port
 }
 
-func (e *Env) BroadcastPortExists() bool {
-	return len(e.broadcast_port) > 0 && len(e.broadcast_public_key) > 0 && len(e.broadcast_secret_key) > 0
-}
-
 // Returns the broadcast port environment variable
 func (e *Env) BroadcastPortEnv() string {
 	return GetString(e.service + "_BROADCAST_PORT")
 }
 
-// Checks whether the request-reply's host and port exists
+// Checks whether the request-reply's host and port exists.
+// If security is enabled, then the function will check for the public key as well.
 func (e *Env) UrlExist() bool {
-	return len(e.port) > 0 && len(e.host) > 0 && len(e.public_key) > 0
+	if !(len(e.port) > 0 && len(e.host) > 0) {
+		return false
+	}
+
+	plain, _ := argument.Exist(argument.PLAIN)
+	if !plain {
+		return len(e.public_key) > 0
+	}
+
+	return true
 }
 
-// Checks whether the port exists
+// Checks whether the port exists.
+// If security is enabled, then the function will check for the public key and secret key as well.
 func (e *Env) PortExist() bool {
-	return len(e.port) > 0 && len(e.public_key) > 0 && len(e.secret_key) > 0
+	if !(len(e.port) > 0) {
+		return false
+	}
+
+	plain, _ := argument.Exist(argument.PLAIN)
+	if !plain {
+		return len(e.public_key) > 0 && len(e.secret_key) > 0
+	}
+
+	return true
 }
 
 // Checks whether the broadcast host and port exists
+// If security is enabled, then the function will check for public key as well.
 func (e *Env) BroadcastExist() bool {
-	return len(e.broadcast_host) > 0 && len(e.broadcast_port) > 0 && len(e.broadcast_public_key) > 0
+	if !(len(e.broadcast_host) > 0 && len(e.broadcast_port) > 0) {
+		return false
+	}
+
+	plain, _ := argument.Exist(argument.PLAIN)
+	if !plain {
+		return len(e.broadcast_public_key) > 0
+	}
+
+	return true
+}
+
+// Checks whether the broadcast port exists.
+// If security is enabled, then the function will check for the public key and secret key as well.
+func (e *Env) BroadcastPortExists() bool {
+	if !(len(e.broadcast_port) > 0) {
+		return false
+	}
+	plain, _ := argument.Exist(argument.PLAIN)
+	if !plain {
+		return len(e.broadcast_public_key) > 0 && len(e.broadcast_secret_key) > 0
+	}
+
+	return true
 }
 
 // Necessary environment variables, to subscribe to the SDS Service
+// Otherwise panic
 func (e *Env) ToSubscribe() *Env {
 	if !e.BroadcastExist() {
 		service := e.DomainName()
@@ -266,6 +333,7 @@ func (e *Env) ToSubscribe() *Env {
 }
 
 // Necessary environment variables, to request to SDS Service.
+// Otherwise panic
 func (e *Env) ToRequest() *Env {
 	if !e.UrlExist() {
 		service := e.DomainName()
@@ -281,7 +349,7 @@ func (e *Env) ToRequest() *Env {
 }
 
 // Necessary environment variables, to broadcast by SDS Service.
-// Panic otherwise.
+// Otherwise panic.
 func (e *Env) ToBroadcast() *Env {
 	if !e.BroadcastPortExists() {
 		service := e.DomainName()
@@ -297,7 +365,7 @@ func (e *Env) ToBroadcast() *Env {
 }
 
 // Necessary environment variables, to reply by SDS Service.
-// Panic otherwise.
+// Otherwise panic.
 func (e *Env) ToReply() *Env {
 	if !e.PortExist() {
 		service := e.DomainName()
